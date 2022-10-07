cb8005275ffe503df241293f9303e39185736214
zhenya25 <zhenya.ku@mail.ru>
2022-10-07 21:29:35 +0600

    Added basic logic

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BaseObject : MonoBehaviour
+{
+    [SerializeField] protected MeshRenderer _meshRenderer;
+
+    public void SetTransformByHit(RaycastHit hit)
+    {
+        // var offset = _meshRenderer.bounds.ClosestPoint(transform.position - hit.point);
+        transform.position = hit.point;
+    }
+    
+}
